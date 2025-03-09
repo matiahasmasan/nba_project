a95f4847f8af7f58c7c48deec5c4daaa06b54ed9
Matia Hasmasan <83250771+matiahasmasan@users.noreply.github.com>
2025-03-09 22:48:29 +0200

    Add files via upload

---
+++ teamData.js
@@ -0,0 +1,124 @@
+const teamData = {
+  ATL: {
+    name: "Hawks",
+    logo: "https://cdn.nba.com/logos/nba/1610612737/global/L/logo.svg",
+  },
+  BOS: {
+    name: "Celtics",
+    logo: "https://cdn.nba.com/logos/nba/1610612738/global/L/logo.svg",
+  },
+  BKN: {
+    name: "Nets",
+    logo: "https://cdn.nba.com/logos/nba/1610612751/global/L/logo.svg",
+  },
+  CHA: {
+    name: "Hornets",
+    logo: "https://cdn.nba.com/logos/nba/1610612766/global/L/logo.svg",
+  },
+  CHI: {
+    name: "Bulls",
+    logo: "https://cdn.nba.com/logos/nba/1610612741/global/L/logo.svg",
+  },
+  CLE: {
+    name: "Cavaliers",
+    logo: "https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg",
+  },
+  DAL: {
+    name: "Mavericks",
+    logo: "https://cdn.nba.com/logos/nba/1610612742/global/L/logo.svg",
+  },
+  DEN: {
+    name: "Nuggets",
+    logo: "https://cdn.nba.com/logos/nba/1610612743/global/L/logo.svg",
+  },
+  DET: {
+    name: "Pistons",
+    logo: "https://cdn.nba.com/logos/nba/1610612765/global/L/logo.svg",
+  },
+  GSW: {
+    name: "Warriors",
+    logo: "https://cdn.nba.com/logos/nba/1610612744/global/L/logo.svg",
+  },
+  HOU: {
+    name: "Rockets",
+    logo: "https://cdn.nba.com/logos/nba/1610612745/global/L/logo.svg",
+  },
+  IND: {
+    name: "Pacers",
+    logo: "https://cdn.nba.com/logos/nba/1610612754/global/L/logo.svg",
+  },
+  LAC: {
+    name: "Clippers",
+    logo: "https://cdn.nba.com/logos/nba/1610612746/global/L/logo.svg",
+  },
+  LAL: {
+    name: "Lakers",
+    logo: "https://cdn.nba.com/logos/nba/1610612747/global/L/logo.svg",
+  },
+  MEM: {
+    name: "Grizzlies",
+    logo: "https://cdn.nba.com/logos/nba/1610612763/global/L/logo.svg",
+  },
+  MIA: {
+    name: "Heat",
+    logo: "https://cdn.nba.com/logos/nba/1610612748/global/L/logo.svg",
+  },
+  MIL: {
+    name: "Bucks",
+    logo: "https://cdn.nba.com/logos/nba/1610612749/global/L/logo.svg",
+  },
+  MIN: {
+    name: "Timberwolves",
+    logo: "https://cdn.nba.com/logos/nba/1610612750/global/L/logo.svg",
+  },
+  NOP: {
+    name: "Pelicans",
+    logo: "https://cdn.nba.com/logos/nba/1610612740/global/L/logo.svg",
+  },
+  NYK: {
+    name: "Knicks",
+    logo: "https://cdn.nba.com/logos/nba/1610612752/global/L/logo.svg",
+  },
+  OKC: {
+    name: "Thunder",
+    logo: "https://cdn.nba.com/logos/nba/1610612760/global/L/logo.svg",
+  },
+  ORL: {
+    name: "Magic",
+    logo: "https://cdn.nba.com/logos/nba/1610612753/global/L/logo.svg",
+  },
+  PHI: {
+    name: "76ers",
+    logo: "https://cdn.nba.com/logos/nba/1610612755/global/L/logo.svg",
+  },
+  PHX: {
+    name: "Suns",
+    logo: "https://cdn.nba.com/logos/nba/1610612756/global/L/logo.svg",
+  },
+  POR: {
+    name: "Trail Blazers",
+    logo: "https://cdn.nba.com/logos/nba/1610612757/global/L/logo.svg",
+  },
+  SAC: {
+    name: "Kings",
+    logo: "https://cdn.nba.com/logos/nba/1610612758/global/L/logo.svg",
+  },
+  SAS: {
+    name: "Spurs",
+    logo: "https://cdn.nba.com/logos/nba/1610612759/global/L/logo.svg",
+  },
+  TOR: {
+    name: "Raptors",
+    logo: "https://cdn.nba.com/logos/nba/1610612761/global/L/logo.svg",
+  },
+  UTA: {
+    name: "Jazz",
+    logo: "https://cdn.nba.com/logos/nba/1610612762/global/L/logo.svg",
+  },
+  WAS: {
+    name: "Wizards",
+    logo: "https://cdn.nba.com/logos/nba/1610612764/global/L/logo.svg",
+  },
+};
+
+export { teamData };
